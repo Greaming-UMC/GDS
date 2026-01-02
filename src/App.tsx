4acc9f10cc11dd890eb6e6afb7b63dd095ae275c
harrysjuns <harrysjuns@gachon.ac.kr>
2026-01-03 02:46:41 +0900

    feat : 디자인 토큰 theme.css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='bg-purple-500 text-white p-4 font-bold text-center'>
+      <h1>히로의 고구마 태우기</h1>
+      <button className="bg-[var(--color-primary)] text-white">버튼</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     dts({ insertTypesEntry: true }),
   ],
   build: {
